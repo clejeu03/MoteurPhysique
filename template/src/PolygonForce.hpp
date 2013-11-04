c475b6b0cbdecd5a9e6c08df54993618b1747c3d
Bastien <bastien@ubuntu.(none)>
2013-11-04 23:22:50 +0100

    Class Polygon ok avec variables membres

--- template/src/PolygonForce.hpp
+++ template/src/PolygonForce.hpp
@@ -1,2 +1,18 @@
 #ifndef _H_IMAC3_POLYGONFORCE_H_
 #define _H_IMAC3_POLYGONFORCE_H_
+
+namespace imac3
+{
+
+class PolygonForce : public Force
+{
+private :
+	const Polygon* m_Polygon;
+	float m_fElasticity;
+	const LeapFrogSolver* m_Solver;
+	float m_fDt;
+}
+
+}
+
+#endif
